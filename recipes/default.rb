34338423f33d62b3314f10682bdc3e34ae5a753d
David Tabachnikov <nethunter@studiosh.com>
2016-11-29 18:48:25 +0200

    Add kodi systemd and xwrapper configuration

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,47 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+apt_repository 'kodi' do
+  uri          'ppa:team-xbmc/ppa'
+  distribution node['lsb']['codename']
+end
+
+apt_package 'xserver-xorg-legacy'
+apt_package 'xorg'
+apt_package 'dbus-x11'
+apt_package 'kodi'
+
+user 'kodi' do
+  shell '/bin/false'
+end
+
+group 'kodi' do
+  members 'kodi'
+end
+
+%w{cdrom audio video plugdev users dialout dip input netdev}.each do |g|
+  group g do
+    action :modify
+    members 'kodi'
+    append true
+  end
+end
+
+template '/etc/systemd/system/kodi.service' do
+  source 'kodi-systemd.erb'
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+
+template '/etc/X11/Xwrapper.config' do
+	source 'xwrapper.erb'
+	owner 'root'
+	group 'root'
+	mode '0755'
+end
+
+service 'kodi' do
+  action [ :enable, :start ]
+end
