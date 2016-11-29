f123d1499fa1b47d3bcbbb77a483ed87e08ce9dd
David Tabachnikov <nethunter@studiosh.com>
2016-11-29 17:35:09 +0200

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# blackmirror
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: blackmirror
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
